6cd5e3e90a86ca853c146dc75b0673a4978f88f2
Peter Kahle <pkahle@kahlilia.org>
2023-10-23 17:27:51 +0000

    working around issues, should work now

--- src/lib.rs
+++ src/lib.rs
@@ -4,24 +4,50 @@ use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
 pub struct MoonPhase {
-    pub phase: Phase,
-    pub moon: Moon,
+    pub phase: f64,
+    phase_name: String,
+    phase_emoji: String,
+    pub illumination: f64,
+}
+
+#[wasm_bindgen]
+impl MoonPhase {
+    #[wasm_bindgen(getter)]
+    pub fn phase_name(&self) -> String {
+        self.phase_name.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_phase_name(&mut self, phase_name: String) {
+        self.phase_name = phase_name;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn phase_emoji(&self) -> String {
+        self.phase_emoji.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_phase_emoji(&mut self, phase_emoji: String) {
+        self.phase_emoji = phase_emoji;
+    }
 }
 #[wasm_bindgen]
 pub fn get_moon_phase() -> MoonPhase {
     let moon = Moon::new(SystemTime::now());
-    let mut phase: Phase;
-        for phase in PHASES.iter() {
-            if moon.phase >= phase.start && moon.phase < phase.end {
-                phase = phase
-            }
+    let mut our_phase: &Phase = &PHASES[0];
+    for phase in PHASES.iter() {
+        if moon.phase >= phase.start && moon.phase < phase.end {
+            our_phase = phase
         }
+    }
     MoonPhase {
-        moon,
-        phase,
+        phase: moon.phase,
+        phase_name: our_phase.name.to_string(),
+        phase_emoji: our_phase.emoji.to_string(),
+        illumination: moon.illumination,
     }
 }
-
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
